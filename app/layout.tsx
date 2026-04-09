c5abecd93cc4147aca03c4a4c3c34f35a2942a56
Mai Nhật Hoàng <62861666+MaiNhatHoangY2001@users.noreply.github.com>
2026-04-09 03:06:43 +0000

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,13 +33,11 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn(
-        "h-full",
-        "antialiased",
-        geistSans.variable,
-        geistMono.variable,
-        "font-sans",
-        inter.variable,
-      )}
+              "h-full",
+              "antialiased",
+              geistSans.variable,
+              geistMono.variable,
+            , "font-sans", inter.variable, geistHeading.variable)}
     >
       <body className="min-h-full flex flex-col">
         <SidebarProvider>
